b11cd2b6935c7a544e075c029287ea0d16fa4aea
WJH <3506456886@qq.com>
2026-04-13 15:22:27 +0800

    feat: 文件属性

---
+++ sandbox/01-文件属性.sh
@@ -0,0 +1,19 @@
+ls -l ./ # long listing format
+<<'COMMENT'
+total 56
+-rw-r--r--  1 a86198  staff   292 Apr 13 13:40 01-修改命令提示符.sh
+-rw-r--r--  1 a86198  staff   578 Apr 13 14:15 01-命令提示.sh
+-rw-r--r--  1 a86198  staff    69 Apr 13 13:57 01-基础命令.sh
+-rw-r--r--  1 a86198  staff   633 Apr 13 15:20 01-文件属性.sh
+-rw-r--r--  1 a86198  staff  2283 Apr 13 15:04 01-文件目录.sh
+drwxr-xr-x  2 a86198  staff    64 Apr 13 15:21 test-files
+-rw-r--r--  1 a86198  staff   590 Apr 13 15:00 智能提示自动补全而不是展示.sh
+-rw-r--r--  1 a86198  staff   823 Apr 13 13:43 配置交互智能提示.sh
+
+文件类型+权限 硬链接数 所有者 所属组 文件大小字节数 最后修改时间 文件名
+
+第一列开头
+- 普通文件
+d 目录文件
+l 符号链接
+COMMENT
